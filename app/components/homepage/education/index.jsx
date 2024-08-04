c425322b5d33d1882c89e09bfcc9943e682e7bcb
Prasanth.k <139969338+prasanth76200@users.noreply.github.com>
2024-08-04 22:22:04 +0530

    Update index.jsx

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -25,11 +25,11 @@ function Education() {
 
       <div className="flex justify-center my-5 lg:py-8">
         <div className="flex  items-center">
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
+          <span className="w-24 h-[2px] bg-[#e15555]"></span>
+          <span className="bg-[#1a1443] border border-[#e15555] w-fit text-white p-2 px-5 text-xl rounded-md">
             Educations
           </span>
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+          <span className="w-24 h-[2px] bg-[#e15555]"></span>
         </div>
       </div>
 
