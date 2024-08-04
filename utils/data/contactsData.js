508d906e85ac743b6d8906fdef00fb0441b9acb1
Prasanth.k <139969338+prasanth76200@users.noreply.github.com>
2024-08-04 12:55:36 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,9 +3,9 @@ export const contactsData = {
     phone: '+91 9952148197',
     address: 'Chennai',
     github: 'https://github.com/prasanth76200',
-    // facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    // facebook: '',
     linkedIn: 'https://www.linkedin.com/in/followprasanth/',
     twitter: 'https://twitter.com/here_prasanth01',
-    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    // stackOverflow: '',
     devUsername: "prasanth_k"
 }
