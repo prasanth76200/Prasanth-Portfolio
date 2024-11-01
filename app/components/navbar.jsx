180d0f17ca6cc0204be426fcf949267b206277cd
Prasanth.k <139969338+prasanth76200@users.noreply.github.com>
2024-11-01 11:32:38 +0530

    Update navbar.jsx

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -35,9 +35,9 @@ function Navbar() {
           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
           </li>
-          <li>
+{/*           <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="https://prasanth-git-hub-portfolio.vercel.app/"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">GITHUB STATS</div></Link>
-          </li>
+          </li> */}
         </ul>
       </div>
     </nav>
